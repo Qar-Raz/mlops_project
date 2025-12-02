2457280a365e5b5fc2f63e5dca0ae7685f30386c
ZainSharjeel <128254747+ZainSharjeel@users.noreply.github.com>
2025-12-02 15:52:18 +0500

    Update __init__.py

--- src/guardrails/__init__.py
+++ src/guardrails/__init__.py
@@ -1,6 +1,1 @@
-# src/guardrails/__init__.py
 
-from .config import config, GuardrailsConfig
-from .safety import InputGuardrails, OutputGuardrails, GuardrailsManager
-
-__all__ = ["config", "GuardrailsConfig", "InputGuardrails", "OutputGuardrails", "GuardrailsManager"]
